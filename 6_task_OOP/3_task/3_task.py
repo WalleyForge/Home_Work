89c23668d60f2684ecd3d7d16285150d8454aab6
WalleyForge <77530284+WalleyForge@users.noreply.github.com>
2021-02-09 21:38:26 +0500

    Add files via upload

---
+++ 6_task_OOP/3_task/3_task.py
@@ -0,0 +1,36 @@
+""" 
+3. Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (должность), income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь, 
+содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}. Создать класс Position (должность) на базе класса Worker. В классе Position реализовать методы получения полного имени сотрудника
+(get_full_name) и дохода с учетом премии (get_total_income). Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+"""
+
+
+
+class Worker: 
+
+    def __init__(self, names, surnames, positions, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+class Position(Worker): 
+    def  get_full_name(self):
+        return self.name + " " + self.surname
+
+
+    def get_total_income(self):
+        return self._income.get("wage") + self._income.get("bonus")
+        
+
+
+name = input("Введите имя: ")
+surname = input("Ввежите Фамилию: ")
+position = input("Укажите Должность: ")
+wage = int(input("Укажите оклад: "))
+bonus = int(input("Укажите премию: "))
+p = Position(name, surname, position,wage,bonus)
+print(p.get_full_name())
+print(p.position)
+print(p.get_total_income())
+
